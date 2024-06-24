d51ea36fbfb9671609802f5467ff2da607d9097b
Duff97 <felduf12@gmail.com>
2024-06-24 16:16:12 -0400

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { PackageProvider } from './providers/PackageProvider.tsx'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics />
     <PackageProvider>
       <App />
     </PackageProvider>
